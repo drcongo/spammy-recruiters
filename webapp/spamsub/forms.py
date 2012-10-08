779ee3a76919ae439b23f99231ebf143a1626c2f
Stephan Hügel <urschrei@gmail.com>
2012-10-08 14:57:24 +0100

    Tidying up form and form template

--- webapp/spamsub/forms.py
+++ webapp/spamsub/forms.py
@@ -6,10 +6,11 @@ reg = re.compile(r"^@[\w\-.]*\.[a-z]{2,4}$")
 
 class SpammerForm(Form):
     address = StringField(u"Address Entry",[
-    validators.DataRequired(
-        message=u"You must enter an address."),
-    validators.Regexp(reg,
-        flags=0,
-        message=u"Invalid address. The address must begin with an '@'.")
+        validators.DataRequired(
+            message=u"You must enter an address."),
+        validators.Regexp(
+            reg,
+            flags=0,
+            message=u"Invalid address. The address must begin with an '@'.")
     ])
     submit = SubmitField()
