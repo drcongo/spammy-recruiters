210b627b5465bf735978a44aeeb4cebcf4fa9b77
Stephan Hügel <urschrei@gmail.com>
2012-11-06 02:16:56 +0000

    JS reformatting

--- webapp/spamsub/static/js/app.js
+++ webapp/spamsub/static/js/app.js
@@ -1,23 +1,20 @@
 $("#SpammerForm").submit(function(event) {
-
     /* stop form from submitting normally */
     event.preventDefault();
-        
-    /* get some values from elements on the page: */
+    /* get some values from elements on the page */
     var $form = $(this),
-        address = $form.find('input[name="address"]').val(),
-        csrf = $form.find('input[name="csrf_token"]').val(),
-        url = $form.attr('action');
-
-    /* Send the data using POST and re-generate the form w/the results */
-    $.post( url, {
+    address = $form.find('input[name="address"]').val(),
+    csrf = $form.find('input[name="csrf_token"]').val(),
+    url = $form.attr('action');
+    /* send the data using POST and re-generate the form w/the results */
+    $.post(url, {
         address: address,
         csrf_token: csrf
-      },
-      function(data) {
-          var content = $(data);
-          $("#SpammerForm").html(content);
-          $form.find('input[name="address"]').focus();
-      }
+        },
+        function(data) {
+            var content = $(data);
+            $("#SpammerForm").html(content);
+            $form.find('input[name="address"]').focus();
+        }
     );
-  });
+});
