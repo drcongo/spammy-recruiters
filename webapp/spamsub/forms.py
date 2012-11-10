1919e72770c804521d4cf8d006c6ee8b65ee144f
Stephan Hügel <urschrei@gmail.com>
2012-11-10 16:08:42 +0000

    Changing the error message copy a bit

--- webapp/spamsub/forms.py
+++ webapp/spamsub/forms.py
@@ -15,12 +15,12 @@ reg = re.compile(r"^[\w\-.]*\.[a-z]{2,4}$")
 
 
 class SpammerForm(Form):
-    error_msg = u""" The address you entered is wrong. Please type the recruiter's 
-address as follows:<br>Everything <em>after</em> the "@" sign, with no spaces.
-<br>For example: <strong>enterprise-weasels.co.uk</strong> """
+    error_msg = u"""The address you tried to add is wrong. Please type it 
+like this:<ul><li>Everything <em>after</em> the "@" sign, with no spaces.</li>
+<li>Example: <strong>enterprise-weasels.co.uk</strong></li></ul>"""
     address = StringField(u"Address Entry",[
         validators.DataRequired(
-            message=u"You must enter an address."),
+            message=u"You have to enter an address."),
         validators.Regexp(
             reg,
             flags=0,
