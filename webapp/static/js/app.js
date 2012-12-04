d6830e49088df5963d3b1a944dd17d0aca74ac01
Stephan Hügel <urschrei@gmail.com>
2012-12-04 18:39:58 +0000

    Remove unnecessary var from form re-population function

--- webapp/static/js/app.js
+++ webapp/static/js/app.js
@@ -34,20 +34,20 @@ function new_recaptcha(){
 
 $("#SpammerForm").submit(function(event) {
     // stop form from submitting normally
+    var $form = $(this);
     event.preventDefault();
     $('#recaptcha_parent').toggle("fast");
-    var $form = $(this),
     // get "successful" form values
     values = $(this).serializeArray();
-    url = $form.attr('action');
+    url = $(this).attr('action');
     // send the data using POST and re-populate the form w/the results
     $.post(
         url,
         values,
         function(data) {
             // TODO we need some 500 error handling here
-            var content = $(data);
-            $("#SpammerForm").html(content);
+            // re-populate the form
+            $form.html($(data));
             // only clear the address field if there are no errors
             if ($("#errors").length !== 0) {
                 $form.find('input[name="address"]').focus();
