0056ad01c2c9bd721983086da37ccc8506d43f53
Stephan Hügel <urschrei@gmail.com>
2012-12-04 21:42:10 +0000

    Rework animations
    We're now making use of the slideUp and slideDown callbacks to trigger
    form submission and reCaptcha refresh

--- webapp/static/js/app.js
+++ webapp/static/js/app.js
@@ -10,54 +10,56 @@ function fix_tabindexes() {
 
 $(document).ready(function() {
     // dramatically reveal recaptcha, and fix tab indexes
-    $("#recaptcha_parent").show("fast");
+    $("#recaptcha_parent").hide();
+    $("#recaptcha_parent").slideDown("slow");
     fix_tabindexes();
 });
 
 
 function new_recaptcha(){
     // render a new recaptcha when the form is re-populated via AJAX
-    Recaptcha.create(
-        $RECAPTCHA_PUBLIC_KEY,
-        "recaptcha",
-        {
-            theme: "clean",
-            tabindex: 0,
-            callback: function(){
-                $("#recaptcha_parent").show("slow");
-                fix_tabindexes();
-            }
-        }
-    );
+    $("#recaptcha_parent").slideDown("fast", function(){
+            Recaptcha.create(
+                $RECAPTCHA_PUBLIC_KEY,
+                "recaptcha",
+                {
+                    theme: "clean",
+                    tabindex: 0,
+                    callback: function(){
+                        fix_tabindexes();
+                    }
+                }
+            );
+    });
 }
 
 
+
 $("#SpammerForm").submit(function(event) {
-    // stop form from submitting normally
-    var $form = $(this);
     event.preventDefault();
-    $('#recaptcha_parent').toggle("fast");
-    // get "successful" form values
-    values = $(this).serializeArray();
-    url = $(this).attr('action');
-    // send the data using POST and re-populate the form w/the results
-    $.post(
-        url,
-        values,
-        function(data) {
-            // TODO we need some 500 error handling here
-            // re-populate the form
-            $form.html($(data));
-            // only clear the address field if there are no errors
-            if ($("#errors").length !== 0) {
-                $form.find('input[name="address"]').focus();
-            }
-            else {
-                $form.find('input[name="address"]').val("").focus();
-            }
-            $("#thanks").delay(10000).fadeOut(750);
-            $("#errors").delay(10000).fadeOut(750);
-            new_recaptcha();
-        }
-    );
+    var $form = $(this);
+    // get "successful" form values and URL
+    values = $form.serializeArray();
+    url = $form.attr('action');
+    $('#recaptcha_parent').slideUp("slow", function(){
+            // send the data using POST and re-populate the form w/the results
+            $.post(
+                url,
+                values,
+                function(data) {
+                    // TODO we need some 500 error handling here
+                    // re-populate the form
+                    $form.html($(data));
+                    // only clear the address field if there are no errors
+                    if ($("#errors").length !== 0) {
+                        $form.find('input[name="address"]').focus();
+                    }
+                    else {
+                        $form.find('input[name="address"]').val("").focus();
+                    }
+                    $(".information").delay(10000).fadeOut(750);
+                    new_recaptcha();
+                }
+            );
+    });
 });
