1bc8c8480b15330ceb769cb10e304e0a38742e6b
Stephan Hügel <urschrei@gmail.com>
2013-03-09 15:38:15 +0000

    Adding a gitignore file

--- webapp/fabfile/virtualenv.py
+++ webapp/fabfile/virtualenv.py
@@ -4,19 +4,28 @@ from fabric.colors import cyan, red
 from fabric.utils import abort
 from utils import do
 
+
 def build():
     """ Build or update the virtualenv """
     with settings(hide('stdout')):
         print(cyan('\nUpdating venv, installing packages...'))
+        # remove non-virtualenv Fabric, because it causes problems
+        # TODO: the logic of this will need to be re-thought
+        do('sudo pip uninstall Fabric -qy', capture=True)
         do('[ -e venv ] || virtualenv venv --no-site-packages')
         # annoyingly, pip prints errors to stdout (instead of stderr), so we
         # have to check the return code and output only if there's an error.
         with settings(warn_only=True):
-            pip = do('venv/bin/pip install -r requirements.txt', capture=True)
-            # remove non-virtualenv pip, because it causes problems
-            # TODO: the logic of this will need to be re-thought
-            remfab = do(
-                'sudo pip uninstall Fabric -qy',
+            # upgrade pip so we can take advantage of fancy new features
+            pupgrade = do('venv/bin/pip install pip --upgrade')
+            if pupgrade.failed:
+                print(red(pupgrade))
+                abort("pip upgrade unsuccessful %i" % pupgrade.return_code)
+            # http://www.pip-installer.org/en/latest/cookbook.html#fast-local-installs
+            do('mkdir pyarchives', capture=True)
+            do('venv/bin/pip install --download pyarchives -r requirements.txt')
+            pip = do(
+                'venv/bin/pip install --no-index --find-links=file://vagrant/pyarchives -r requirements.txt --upgrade',
                 capture=True)
         if pip.failed:
             print(red(pip))
