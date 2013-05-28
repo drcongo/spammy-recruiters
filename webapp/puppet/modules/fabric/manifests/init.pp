33568bbd285dc837791d8ccdb08ae127a8d8f8d1
Stephan Hügel <stephan.hugel.12@ucl.ac.uk>
2013-05-28 14:05:04 +0100

    Puppet-install PsycoPG2 so we can migrate outside venv

--- webapp/puppet/modules/fabric/manifests/init.pp
+++ webapp/puppet/modules/fabric/manifests/init.pp
@@ -3,6 +3,10 @@ class fabric {
     provider => 'pip',
     ensure   => 'present',
   }
+  package { 'psycopg2':
+    provider => 'pip',
+    ensure   => 'present',
+  }
   package { 'Fabric':
     provider => 'pip',
     ensure   => 'present',
